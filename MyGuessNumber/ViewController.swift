707d652153a56d121a6b57e4b039d0ee8d72dc9b
jiangzongyi <reavter103@gmail.com>
2017-09-08 16:17:03 +0800

    v2

--- MyGuessNumber/ViewController.swift
+++ MyGuessNumber/ViewController.swift
@@ -11,44 +11,48 @@ import Cocoa
 class ViewController: NSViewController {
 
     var answer = ""
+    let n = 4
+    var counter = 0
     
     @IBOutlet weak var guess: NSTextField!
-    
     @IBOutlet weak var hist: NSTextField!
     
-    
-    
     @IBAction func doGuess(_ sender: Any) {
+        counter += 1
         let gvalue = guess.stringValue
-        let result = checkAB(guess: guess.stringValue, answer: answer)
-        hist.stringValue += gvalue + " => " + result + "\n"
+        let result = checkAB(guess: gvalue, answer: answer)
+        hist.stringValue += "\(counter). \(gvalue) => \(result)\n"
         
-        if result == "3A0B" {
+        if result == "\(n)A0B" {
         showDilaog(title: "恭喜", mesg: result)
+            initGames()
+        }else if counter == 10 {
+            showDilaog(title: "9487", mesg: "答案為\(answer)")
+            guess.stringValue = ""
+        
         }else {
             showDilaog(title: "結果", mesg: result)
             guess.stringValue = ""
         }
-        
-        
     }
     
-    
-    
     @IBAction func doReset(_ sender: Any) {
-        
+        initGames()
         
     }
     
     func initGames (){
-        answer = createAnswer()
+        answer = createAnswer(number: n)
+        guess.stringValue = ""
+        hist.stringValue = ""
+        counter = 0
         print(answer)
     }
     
     func showDilaog(title: String, mesg: String) {
         let alert = NSAlert()
-        alert.informativeText = title
-        alert.messageText = mesg
+        alert.informativeText = mesg
+        alert.messageText = title
         alert.alertStyle = NSAlertStyle.informational
         alert.addButton(withTitle: "知道了")
         alert.runModal()
@@ -60,7 +64,11 @@ class ViewController: NSViewController {
     
     func checkAB(guess:String, answer:String) -> String {
         let len = guess.characters.count
+        
+        if len != 4 {return "別鬧了"}
+        
         var A = 0, B = 0
+        
         for i in 0..<len {
             let gchar = mysubstrv2(source: guess, from: i, to: i+1)
             let achar = mysubstrv2(source: answer, from: i, to: i+1)
@@ -94,11 +102,11 @@ class ViewController: NSViewController {
     
     
     
-    func createAnswer() -> String {
-        var num = Array(0 ... 9)
+    func createAnswer(number:Int) -> String {
+        var num = Array((0 ... 9))
         num = shullfe(source: Array(num))
         var ret = ""
-        for v in 0 ... 2 {
+        for v in 0..<number {
             ret += String(num[v])
         }
         return ret
