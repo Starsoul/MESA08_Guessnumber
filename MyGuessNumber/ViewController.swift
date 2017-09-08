2d06ca656d039af50f6fe09a9991133e394f0c5d
jiangzongyi <reavter103@gmail.com>
2017-09-08 13:35:40 +0800

    Initial Commit

---
+++ MyGuessNumber/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyGuessNumber
+//
+//  Created by 江宗益 on 2017/9/8.
+//  Copyright © 2017年 江宗益. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
